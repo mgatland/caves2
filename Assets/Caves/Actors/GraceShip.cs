b826774d5675c269ae059da713b2f5d6e765add4
mgatland <mgatland@gmail>
2015-03-24 14:46:40 +1300

    New player character - a flying ship. Remove the old platform game character

---
+++ Assets/Caves/Actors/GraceShip.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraceShip : MonoBehaviour {
+
+	private Rigidbody2D m_Rigidbody2D;
+	private static float m_MaxSpeed = 300f;
+	private bool m_FacingRight = true;
+	
+	void Awake () {
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	public void Move(float moveX, float moveY, bool shoot)
+	{			
+		// Move the character
+		m_Rigidbody2D.velocity = new Vector2(moveX*m_MaxSpeed, moveY*m_MaxSpeed);
+		
+		// If the input is moving the player right and the player is facing left...
+		if (moveX > 0 && !m_FacingRight)
+		{
+			Flip();
+		}
+		// Otherwise if the input is moving the player left and the player is facing right...
+		else if (moveX < 0 && m_FacingRight)
+		{
+			Flip();
+		}
+	}
+
+	private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+		
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+}
---
+++ Assets/Caves/Actors/GraceUserControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (GraceShip))]
+public class GraceUserControl : MonoBehaviour {
+	
+	private GraceShip m_Character;
+	private bool m_Shoot;
+	
+	
+	private void Awake()
+	{
+		m_Character = GetComponent<GraceShip>();
+	}
+	
+	private void Update()
+	{
+		if (!m_Shoot)
+		{
+			// Read the shoot input in Update so button presses aren't missed.
+			m_Shoot = CrossPlatformInputManager.GetButtonDown("Fire1");
+		}
+	}
+	
+	
+	private void FixedUpdate()
+	{
+		float h = CrossPlatformInputManager.GetAxis("Horizontal");
+		float v = CrossPlatformInputManager.GetAxis("Vertical");
+		m_Character.Move(h, v, m_Shoot);
+		m_Shoot = false;
+	}
+
+}
